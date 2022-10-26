f26fdc103b981310862dc1adc958b3366f7d3d35
李帅兵 <1679018142@qq.com>
2022-10-27 00:00:43 +0800

    add check cookies

--- src/main/java/edu/uestc/community/controller/IndexController.java
+++ src/main/java/edu/uestc/community/controller/IndexController.java
@@ -1,10 +1,37 @@
 package edu.uestc.community.controller;
 
+import edu.uestc.community.mapper.UserMapper;
+import edu.uestc.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){ return "index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null){
+            return "index";
+        }
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.finByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+
+        return "index";}
 }
--- src/main/java/edu/uestc/community/mapper/UserMapper.java
+++ src/main/java/edu/uestc/community/mapper/UserMapper.java
@@ -9,4 +9,7 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Insert("insert into Users (name, account_id, token, gmt_create, gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User uesr);
+
+    @Select("select * from Users where token = #{token}")
+    User finByToken(String token);
 }
