e405b6cc390e81a3cd8909afc5b83107afa4626e
李帅兵 <1679018142@qq.com>
2022-10-26 23:28:40 +0800

    add h2.db indefei

--- src/main/java/edu/uestc/community/controller/AuthorizeController.java
+++ src/main/java/edu/uestc/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package edu.uestc.community.controller;
 
 import edu.uestc.community.dto.AccessTokenDTO;
 import edu.uestc.community.dto.GithubUser;
+import edu.uestc.community.mapper.UserMapper;
+import edu.uestc.community.model.User;
 import edu.uestc.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,14 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-
+    @Autowired
+    private UserMapper userMapper;
     @Value("${github.client.id}")
     private String clientId;
     @Value("${github.client.secret}")
@@ -25,7 +30,8 @@ public class AuthorizeController {
     private String redirectUrl;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) throws IOException {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -33,8 +39,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_url(redirectUrl);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            // 登录成功，写cookie 和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        }else{
+            // 登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/edu/uestc/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package edu.uestc.community.mapper;
+
+import edu.uestc.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into Users (name, account_id, token, gmt_create, gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User uesr);
+}
