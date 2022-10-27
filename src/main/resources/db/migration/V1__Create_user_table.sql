976bcb0d369dc718a5a3c0391c99f669fa2024a3
李帅兵 <1679018142@qq.com>
2022-10-27 20:16:27 +0800

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,11 @@
+create table USERS
+(
+    ID           INTEGER auto_increment
+        primary key,
+    ACCOUNT_ID   CHARACTER VARYING(50),
+    NAME         CHARACTER VARYING(50),
+    TOKEN        CHARACTER(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT
+);
+
